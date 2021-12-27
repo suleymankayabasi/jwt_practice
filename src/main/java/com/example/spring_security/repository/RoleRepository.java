032ff070a777d5fed26d4363863d129e3fff5bbf
Süleyman Kayabaşı <82765835+suleymankayabasi@users.noreply.github.com>
2021-12-27 03:13:23 +0300

    Initial Commit

---
+++ src/main/java/com/example/spring_security/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.example.spring_security.repository;
+
+import com.example.spring_security.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role,Long> {
+
+    Role findByName(String name);
+}
