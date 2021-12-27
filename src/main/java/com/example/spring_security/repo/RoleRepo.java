41ec0f3aaf6c0091f01007eff85bbc38178b5f61
Süleyman Kayabaşı <82765835+suleymankayabasi@users.noreply.github.com>
2021-12-27 20:09:28 +0300

    Fix Bugs

---
+++ src/main/java/com/example/spring_security/repo/RoleRepo.java
@@ -0,0 +1,8 @@
+package com.example.spring_security.repo;
+
+import com.example.spring_security.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepo extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
